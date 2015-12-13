5eee027aa9a5cfa405a06a2de12384709db9f60f
zhou-yg <zhou.y.g890@gmail.com>
2015-12-13 22:57:54 +0800

    近大远小

--- public/js/hitRunMan/sprites/distanceDisplay.js
+++ public/js/hitRunMan/sprites/distanceDisplay.js
@@ -13,19 +13,19 @@ var text = new PIXI.Text('距离米', {
 text.x = 220;
 text.y = 730;
 
-
 var renderMaxCount = foot.renderMaxNum;
 var renderCount = renderMaxCount;
 var distance = 10;
 
 var calculateDistance = function(distance,speed,footSpeed){
+    var gap = (speed - footSpeed ) * 2;
 
     distance = parseFloat(distance);
 
-    distance += (speed - footSpeed ) * 2;
+    distance += distance <= 1000 ?gap:
+        gap<0?gap:1;
 
-    distance = distance > 1000?distance+1:
-        distance <= 0 ? 0:distance;
+    distance = distance <= 0 ? 0:distance;
 
     distance = distance.toFixed(1);
 
@@ -37,6 +37,9 @@ text.render = function () {
     if(!--renderCount){
 
         distance = calculateDistance(distance,runMan.speed,foot.speed);
+
+        runMan.setMode(distance);
+
 
         renderCount = renderMaxCount;
     }
--- public/js/hitRunMan/sprites/runMan.js
+++ public/js/hitRunMan/sprites/runMan.js
@@ -4,7 +4,6 @@
 var sprite = require('./../../sprite');
 var R = require('../resource');
 
-
 var foot = require('./foot');
 
 var runMan = sprite.getMc({
@@ -21,6 +20,19 @@ runMan.play();
 //固定数值
 runMan.speed = 1.5;
 
+var initialY = 400;
+
+//根据距离差，近大远小
+runMan.setMode = function (distance) {
+    if(distance>=600){
+        distance = 600;
+    }
+
+    var s = 0.9-distance/1000;
+
+    this.scale.set(s,s);
+    this.y = initialY - distance/10;
+};
 
 runMan.render = function () {
 };
