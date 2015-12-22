dca3cba0e04fed7f0ab91bab62aa3f61d7a1b3a0
zhou-yg <zhou.y.g890@gmail.com>
2015-12-22 23:35:44 +0800

    clear

--- public/js/hitRunMan/resource.js
+++ public/js/hitRunMan/resource.js
@@ -20,19 +20,19 @@ var localResource = {
 Object.defineProperty(localResource,'next',{
   enumerable:false,
   value:[{
-    hand:'/images/hitRunMan/hand/hand.png',
-    ttt:'/images/hitRunMan/hand/ttt.png',
-    leftFoot:'/images/hitRunMan/foot/leftFoot.json',
-    rightFoot:'/images/hitRunMan/foot/rightFoot.json',
-    runMan:'/images/hitRunMan/runMan/runMan.json',
-    fist:'/images/hitRunMan/fist/fist.png',
-    gold:'/images/hitRunMan/gold/gold.json',
-    pa:'/images/hitRunMan/pa/pa.png',
-    house1:'/images/hitRunMan/house/house1.png',
-    house2:'/images/hitRunMan/house/house2.png',
-    house3:'/images/hitRunMan/house/house3.png',
+    hand:localResource.hand,
+    ttt:localResource.ttt,
+    leftFoot:localResource.leftFoot,
+    rightFoot:localResource.rightFoot,
+    runMan:localResource.runMan,
+    fist:localResource.fist,
+    gold:localResource.gold,
+    pa:localResource.pa,
+    house1:localResource.house1,
+    house2:localResource.house2,
+    house3:localResource.house3,
   },{
-    flyFist:'/images/hitRunMan/flyFist/flyFist.png',
+    flyFist:localResource.flyFist,
   }]
 })
 
