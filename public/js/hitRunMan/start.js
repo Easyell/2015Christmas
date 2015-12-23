fb65b42473bf700c94e0814d6d66fa75e44ff020
zhou-yg <zhou.y.g890@gmail.com>
2015-12-23 22:39:04 +0800

    lots tree

--- public/js/hitRunMan/start.js
+++ public/js/hitRunMan/start.js
@@ -5,8 +5,16 @@ module.exports = function(render){
   var ready = require('./../loader');
   ready(require('./resource'),function (com) {
     var startStage = new PIXI.Container();
+
     var startSnowMan = require('./sprites/startSnowMan')
+
+    var christmasTree = require('./sprites/christmasTree');
+
+
+    startStage.addChild(christmasTree)
     startStage.addChild(startSnowMan)
+
+
     render(startStage);
   })
 }
