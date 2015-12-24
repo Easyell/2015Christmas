90dc740401f2936391db4d90a8a4acc5344d279e
guoshencheng <648772021@qq.com>
2015-12-25 01:44:53 +0800

    add cdn

--- public/js/hitRunMan/resource.js
+++ public/js/hitRunMan/resource.js
@@ -3,26 +3,26 @@
  */
 
 var localResource = {
-  leftFoot:'/images/hitRunMan/foot/leftFoot.json',
-  rightFoot:'/images/hitRunMan/foot/rightFoot.json',
+  leftFoot:'http://7u2min.com1.z0.glb.clouddn.com/Users%2Fzyg%2FDocuments%2F2015Christmas%2FuploadDir%2F1512250131%2Ffoot%2FleftFoot.json',
+  rightFoot:'http://7u2min.com1.z0.glb.clouddn.com/Users%2Fzyg%2FDocuments%2F2015Christmas%2FuploadDir%2F1512250131%2Ffoot%2FrightFoot.json',
   runMan:'/images/hitRunMan/runMan/runMan.json',
-  gold:'/images/hitRunMan/gold/gold.json',
+  gold:'http://7u2min.com1.z0.glb.clouddn.com/Users%2Fzyg%2FDocuments%2F2015Christmas%2FuploadDir%2F1512250131%2Fgold%2Fgold.json',
   pa:'/images/hitRunMan/pa/pa.png',
   house1:'/images/hitRunMan/house/house1.png',
   house2:'/images/hitRunMan/house/house2.png',
   house3:'/images/hitRunMan/house/house3.png',
-  playBg:'/images/hitRunMan/background/playBackground.png',
+  playBg:'http://7u2min.com1.z0.glb.clouddn.com/Users%2Fzyg%2FDocuments%2F2015Christmas%2FuploadDir%2F1512250131%2Fbackground%2FplayBackground.png',
   start_snowman:'/images/hitRunMan/start_snowman/start_snowman.json',
   startTitle:'/images/hitRunMan/startTitle.png',
   startBackground:'/images/hitRunMan/startBackground/startBackground.png',
-  gift:'/images/hitRunMan/gift/gift.json',
+  gift:'http://7u2min.com1.z0.glb.clouddn.com/Users%2Fzyg%2FDocuments%2F2015Christmas%2FuploadDir%2F1512250131%2Fgift%2Fgift.json',
   snowmanhand:'/images/hitRunMan/snowmanhand/snowmanhand.png',
   howToShare:'/images/hitRunMan/background/howToShare.png',
   playAgain:'/images/hitRunMan/playAgain/playAgain.png',
   shareButton:'/images/hitRunMan/playAgain/share.png',
   resultSuccess:'/images/hitRunMan/playAgain/resultSuccess.png',
   resultFail:'/images/hitRunMan/playAgain/resultFail.png',
-
+  endSign:'//images/hitRunMan/background/endSign.png',
   startBgm:'/audio/start_bgm.mp3',
   gameBgm:'/audio/hot_run.mp3',
 
@@ -45,7 +45,7 @@ var localResource = {
     playBg:'/images/hitRunMan/background/playBackground.png',
   },
   end:{
-    howToShare:'/images/hitRunMan/background/howToShare.png',
+    howToShare:'http://7u2min.com1.z0.glb.clouddn.com/Users%2Fzyg%2FDocuments%2F2015Christmas%2FuploadDir%2F1512250131%2Fbackground%2FhowToShare.png',
   }
 };
 
@@ -56,6 +56,7 @@ var cdn1Resource = {
   runMan:'/images/hitRunMan/runMan/runMan.json',
   gold:'/images/hitRunMan/gold/gold.json',
   pa:'/images/hitRunMan/pa/pa.png',
+  endSign:'http://7u2min.com1.z0.glb.clouddn.com/Users%2Fzyg%2FDocuments%2F2015Christmas%2FuploadDir%2F1512250131%2Fbackground%2FendSign.png',
   house1:'/images/hitRunMan/house/house1.png',
   house2:'/images/hitRunMan/house/house2.png',
   house3:'/images/hitRunMan/house/house3.png',
@@ -72,7 +73,7 @@ var cdn1Resource = {
   resultFail:'/images/hitRunMan/playAgain/resultFail.png',
 
   startBgm:'/audio/start_bgm.mp3',
-  gameBgm:'/audio/hot_run.mp3',
+  gameBgm:'http://7u2min.com1.z0.glb.clouddn.com/Users%2Fzyg%2FDocuments%2F2015Christmas%2FuploadDir%2F1512250131%2Fhot_run.mp3',
 
   start:{
     start_snowman:'/images/hitRunMan/start_snowman/start_snowman.json',
