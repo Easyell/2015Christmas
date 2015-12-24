33ee86a610e61ee73b5d08481347bba273c9c8e4
guoshencheng <648772021@qq.com>
2015-12-25 02:37:26 +0800

    + random () * 20

--- public/js/hitRunMan/gameover.js
+++ public/js/hitRunMan/gameover.js
@@ -7,6 +7,7 @@ var isReady = false;
 module.exports = function (render, success, score) {
     var readyFn = function () {
         isReady = true;
+        score += parseInt(Math.random * 20)
         var container = new PIXI.Container();
         var sign = require('./sprites/sign');
         var share = require('./sprites/share')
