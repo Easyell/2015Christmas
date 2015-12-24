836f668966b90bc7bc798ae884111846369f2ace
guoshencheng <648772021@qq.com>
2015-12-24 22:43:26 +0800

    truely slow speed

--- public/js/hitRunMan/sprites/foot.js
+++ public/js/hitRunMan/sprites/foot.js
@@ -68,15 +68,15 @@ foot.addChild(rightFoot);
 var renderCount = foot.renderMaxNum;
 
 foot.render = function () {
+  if(this.footNum >= 0.1) {
+    this.footNum -= 0.1
+  } else {
+    this.footNum = 0
+  }
     if(!(--renderCount)){
         renderCount = foot.renderMaxNum
         //移动速度，基本0.2,要非常快才有0.3。
         this.speed = foot.footNum/renderCount * 10;//20;
-        if(this.footNum > 0) {
-          this.footNum --
-        } else {
-          this.footNum = 0
-        }
     }
 };
 
